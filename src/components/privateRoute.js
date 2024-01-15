fc7c4323160b38b61484573603ef1e4ceffc478d
FChatelot <florianchatelot@gmail.com>
2024-01-15 15:53:18 +0100

    outlet

--- src/components/privateRoute.js
+++ src/components/privateRoute.js
@@ -1,8 +1,10 @@
 import{ Navigate, Outlet } from "react-router-dom";
 import {useSelector} from "react-redux";
 // la private route nous permet de sécuriser l'acces au profil. Si il n'y a pas de jeton, je ne peux pas y accéder en copiant le lien du profile.
-const PrivateRoute = () => {
-    const { userInfo } = useSelector((state) => state.auth);
-    return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
-  };
-  export default PrivateRoute;
+export default function PrivateRoute() {
+    const { userInfo } = useSelector((state)=>state.auth);
+    return (
+        userInfo ? <Outlet/> : <Navigate to="/login" replace/>
+        )
+}
+    
