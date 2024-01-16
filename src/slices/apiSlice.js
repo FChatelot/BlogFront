05b168ebf8ab6e5d10a5de61f46c7289a7fe1793
FChatelot <florianchatelot@gmail.com>
2024-01-16 17:21:14 +0100

    test

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,9 +1,10 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:4000" });//récupération de nos infos back via l'url serverside
+const baseQuery = fetchBaseQuery({ mode:"cors", baseUrl: "http://localhost:4000" });//récupération de nos infos back via l'url serverside
 
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ['User'], //réqupération de la bdd qu'on veut utiliser.
   endpoints: (builder) => ({}),
 });
+//https://stackoverflow.com/questions/72413353/redux-toolkit-rtk-query-cors-issue
