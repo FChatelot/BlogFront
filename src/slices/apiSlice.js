0ef3531941a4294a1dae6f2de1c189c53850ed1f
FChatelot <florianchatelot@gmail.com>
2024-01-17 15:35:24 +0100

    test

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,6 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "https://shielded-eyrie-12790-2606840fb7b6.herokuapp.com" });//récupération de nos infos back via l'url serverside 
+const baseQuery = fetchBaseQuery({ baseUrl: "https://shielded-eyrie-12790-2606840fb7b6.herokuapp.com" || "http://localhost:4000" });//récupération de nos infos back via l'url serverside "http://localhost:4000"
 
 export const apiSlice = createApi({
   baseQuery,
