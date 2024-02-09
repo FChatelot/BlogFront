42cf0a68992b37eaf2a32eb8c5a25aebca5db72c
FChatelot <florianchatelot@gmail.com>
2024-02-09 15:06:49 +0100

    add security local storage info

--- src/slices/authSlice.js
+++ src/slices/authSlice.js
@@ -1,7 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import  secureLocalStorage  from  "react-secure-storage";//pour hash les données de notre local storage
 //sotckage de nos données utilisateurs dans le cookie.
 const initialState = {
-  userInfo: localStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")): null,
+  userInfo: secureLocalStorage.getItem("userInfo") ? JSON.parse(localStorage.getItem("userInfo")): null,
 };
 
 const authSlice = createSlice({
@@ -10,11 +11,11 @@ const authSlice = createSlice({
   reducers: {
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
-      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      secureLocalStorage.setItem("userInfo", JSON.stringify(action.payload));
     },
     logout: (state, action) => {
       state.userInfo = null;
-      localStorage.removeItem("userInfo");
+      secureLocalStorage.removeItem("userInfo");
     },
   },
 });
